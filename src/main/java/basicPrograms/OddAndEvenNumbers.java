a4863578f677ef5e8de4557e24e5631d1dea9b95
Mohammad Haque <mohammadhaque898@gmail.com>
2022-04-24 13:11:37 -0400

    added more code

---
+++ src/main/java/basicPrograms/OddAndEvenNumbers.java
@@ -0,0 +1,21 @@
+package basicPrograms;
+
+public class OddAndEvenNumbers {
+    public static void main(String[] args) {
+
+
+        int number=52;
+
+        if (number%2 ==0)
+            System.out.println("Even Numbers:" +number);
+
+
+        //1,3,5,7
+        else
+
+       // if (number%2 !=0)
+        System.out.println("Odd Numbers:" + number);
+       //2,4,6,
+
+        }
+}
